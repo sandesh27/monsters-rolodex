00cc4dc0763891616113c752c63fea6e65411c36
sandesh magdum <sandesh.s_magdum@yahoo.co.in>
2019-07-23 15:04:16 +0530

    Updates

---
+++ precache-manifest.e62e7120ed2d6c18fbbea91027236a05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352281ae0565f910915d6cfc3bafed71",
+    "url": "/mosnters-rolodex/index.html"
+  },
+  {
+    "revision": "4ce17415c784de94ae0e",
+    "url": "/mosnters-rolodex/static/css/main.287df9e2.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/mosnters-rolodex/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "4ce17415c784de94ae0e",
+    "url": "/mosnters-rolodex/static/js/main.813a9f9a.chunk.js"
+  },
+  {
+    "revision": "69437a5b748dc9d38bfc",
+    "url": "/mosnters-rolodex/static/js/runtime~main.1c02d9b6.js"
+  }
+]);
